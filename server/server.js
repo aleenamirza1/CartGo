bd72c9f3d50f32fa0401dc094f557c2b48c65822
aleenamirza1 <aleenamirza843@gmail.com>
2026-01-18 17:20:34 +0500

    frontend api added

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://cart-go-frontend.vercel.app/']
 
 app.post('/stripe' , express.raw({type: 'application/json'}), stripeWebHooks)
 
